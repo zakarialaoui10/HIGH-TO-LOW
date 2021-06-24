e6d99c286169a89ccc116da42f203b325eb06aeb
zakariaalaoui10 <63521765+zakariaalaoui10@users.noreply.github.com>
2021-06-24 14:35:05 +0100

    Update 2-2-nested if.asm

--- C vs Assembly/2-2-nested if.asm
+++ C vs Assembly/2-2-nested if.asm
@@ -1,9 +1,12 @@
 li $t0,1  #a
 li $t1,4  #b
 beq $t0,1,if
-j fin
+j print
 if:
   la $t2,($t0)   #c
-  beq $t1,3,fin 
+  beq $t1,3,print 
   add $t2,$t2,$t1
-fin:
+print:
+li $v0,1
+move $a0,$t2
+syscall 
