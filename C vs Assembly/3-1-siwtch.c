5d34f57c0e7e8bc816277f43124de96932b7b565
zakariaalaoui10 <63521765+zakariaalaoui10@users.noreply.github.com>
2021-06-25 19:29:01 +0100

    Update 3-1-siwtch.c

--- C vs Assembly/3-1-siwtch.c
+++ C vs Assembly/3-1-siwtch.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
-int a=1;
+int a;
 int b=0;
+scanf("%d",&a);
 switch(a){
-	case 0:b=-1;break;
-	case 1:b=0;break;
-	default:b=1;break;
+	case 0:b=-1; break;
+	case 1:b=0; break;
+	default :b=1; break;
 }
 printf("%d",b);
 }
